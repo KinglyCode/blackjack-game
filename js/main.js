76be2cca4642f109f3d8b3c460dc67dbf08ae9ed
KinglyCode <bayglory@yahoo.com>
2022-09-19 17:53:38 -0700

    adds cards adds a few more html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 /*----- constants -----*/
+const dSum = 0
+const pSum = 0
+const pHit = true
+
 
 
   /*----- state variables -----*/
 
+  let dHiddenCard, player, winner
 
   /*----- cached elements  -----*/
-
+const mDisplayEl = document.querySelector('h2')
+const resetBtnEl = document.querySelector('button')
 
   /*----- event listeners -----*/
 
