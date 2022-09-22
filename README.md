e70fa0011a27e9121391cb5a2097363ed76f2520
KinglyCode <bayglory@yahoo.com>
2022-09-22 16:30:05 -0700

    updates main.js, index and main.css

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+Hello and Welcome to my really simple Blackjack game!
+
+Project BlackJack
+
+Like I said above, this is a truly simple game of BlackJack. There's no double down or splitting cards, not even points or betting. This was just a little project and my first time coding a full game with HTML, CSS, and JavaScript.
+
+If you haven't played BlackJack, it's just a simple numbers game. You have to beat the dealer. You have to try to get more numbers than the dealer or hope the dealers busts (has more than 21) and you win! Of course, if you're not careful, you can bust and lose as well!
+
 # blackjack-game
--- js/main.js
+++ js/main.js
@@ -1,41 +1,29 @@
-/*----- constants -----*/
-let suits = ['h', 's', 'd', 'c']
-//let suits = ['hearts', 'spades', 'diamonds', 'clubs']
-let values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K']
-//let values = ['A', 'r02', 'r03', 'r04', 'r05', 'r06', 'r07', 'r08', 'r09', 'r10', 'J', 'Q', 'K']
-///let dealerFaceCards = document.getElementsByClassName('dealer-cards')
 
-  /*----- state variables -----*/
+let suits = ['h', 's', 'd', 'c']
+let values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K']
 
 let winner
 let dealerSum = 0
 let playerSum = 0
 let dCard = []
-let pCard =[]
+let pCard = []
 let cardDeck = []
-
-
-  /*----- cached elements  -----*/
+let playerStay = false
 
 const mDisplayEl = document.getElementById('message-box')
 const startButtonEl = document.getElementById('start-btn')
 const hitButtonEl = document.getElementById('hit-btn')
 const stayButtonEl = document.getElementById('stay-btn')
 
+const playerSumDisplay = document.getElementById('p-sum')
 const playerBoardEl = document.getElementById('player-cards')
 const dealerBoardEl = document.getElementById('dealers-cards')
-
-  /*----- event listeners -----*/
 
 startButtonEl.addEventListener('click', handlePlayClick)
 hitButtonEl.addEventListener('click', handleHitClick)
 stayButtonEl.addEventListener('click', handleStayClick)
 
-
-  /*----- functions -----*/
-
   function init() {
-    winner = null
     buildDeck()
   }
 
@@ -62,14 +50,17 @@ stayButtonEl.addEventListener('click', handleStayClick)
 
   }
 
-
   function gameStart() {
     pCard = []
     dCard = []
     shuffleDeck()
-    //playerBoard()
     dealNew()
     render()
+  }
+
+  function calculateHitCard() {
+    render(pCard)
+  
   }
 
   function dealNew(){
@@ -80,62 +71,70 @@ stayButtonEl.addEventListener('click', handleStayClick)
   }
 
   function render() {
-    //Loop through player and dealer hands
-    //For each card in the hand, create a div and apply the class
-    //Append it to the DOM
+    dealerBoardEl.innerHTML = ""
+    playerBoardEl.innerHTML = ""
 
-    playerBoardEl.forEach(cardDeck)
+    dCard.forEach(card => {
+      let dealerCardEl = document.createElement('div')
+      dealerCardEl.classList.add(`${card.face}`, `card`)
+      dealerBoardEl.appendChild(dealerCardEl)
+    })
+
+    pCard.forEach(card => {
+      let playerCardEl = document.createElement('div')
+      playerCardEl.classList.add(`${card.face}`, `card`)
+      playerBoardEl.appendChild(playerCardEl)
+    })
 
   }
   
-  function calculateCards () {
-    //For each card in the dealers and players hand, add the sum.
+  function calculateCards (hand) {
+    let total = 0
+    hand.forEach(card => {
+      total += card.value 
+    })
+    return total
   }
 
   function checkWin() {
-    //If player equal to 21, they win
-    //Else if the player has less 17 and dealer has more than 17, dealer wins, update
-    //DOM with Dealer Wins
-    //Else if the dealer has less than 17 and player has more than 17, player wins
-    //update DOM to display Player Wins
-    //otherwise, 
-
-    if(playerSum === 21){
-        mDisplayEl.innerHTML = 'You Win!' 
-      }else if(playerSum > 21){
-        mDisplayEl.innerHTML = 'Player Busts!'
-      }else if(dealerSum > 21){
+    if(playerSum > 21){
+      mDisplayEl.innerHTML = 'Player Busts!'
+    }else if(playerSum === 21){
+      mDisplayEl.innerHTML = 'BlackJack!'
+    }else if(playerStay && dealerSum > 21){
         mDisplayEl.innerHTML = 'Dealer Busts'
-      }else if(playerSum < 17 && dealerSum > playerSum){
+      }else if(playerStay && dealerSum > playerSum){
         mDisplayEl.innerHTML = 'Dealer Wins!'
-      }else if(dealerSum < 17 && playerSum > dealerSum){
+      }else if(playerStay && dealerSum < playerSum){
         mDisplayEl.innerHTML = 'Player Wins!'
-      }else{
+      }else if (playerStay && dealerSum === playerSum){
         mDisplayEl.innerHTML = 'Push!'
       }
-    }
-  
-  
+  }
     
-
+  function calculateDealerHand() {
+    while(dealerSum < 17){
+      dCard.push(cardDeck.pop())
+      dealerSum = calculateCards(dCard)
+    }
+  }
+    
   function handlePlayClick() {
     gameStart()
-    console.log(handlePlayClick)
-
+    playerSum = calculateCards(pCard)
   }
 
   function handleHitClick() {
-    render()
-    calculateCards()
-    console.log(handleHitClick)
-
+    pCard.push(cardDeck.pop())
+    playerSum = calculateCards(pCard)
+    calculateHitCard(pCard)
   }
 
   function handleStayClick() {
+    playerStay = true
+    calculateDealerHand()
+    render()
     checkWin()
-    calculateCards()
-    console.log(handleStayClick)
-
   }
 
   init()
